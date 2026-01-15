14ed5fd3bf208520d478ce21c39fc2e344432760
harshita <harshitasurana2005@gmail.com>
2026-01-15 13:13:13 +0530

    database setup for authentication

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,16 @@
 import express from 'express'
+import mongoose from 'mongoose'
+
 
 const app=express()
-
+const connectDb=async ()=>{
+    mongoose.connect('mongodb://localhost:27017/E-Commerce')
+    const signUpSchema=new mongoose.Schema({});
+    const signUp=mongoose.model('signUp',signUpSchema)
+    const data=await signUp.find()
+    console.warn(data)
+}
+connectDb
 app.get('/',(req,res)=>{
     res.send('hello')
 })
